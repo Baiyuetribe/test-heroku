fb4f56fe4c99eee3c84919321db504b8e98a202d
佰阅部落 <43716063+Baiyuetribe@users.noreply.github.com>
2021-06-30 22:56:55 +0800

    t

--- n.sh
+++ n.sh
@@ -9,4 +9,32 @@ sed -i "s|'# 处理文件夹'|'sed -i \"s\|'sqlite:///'+os.path.join(SQL_PATH,'k
 sed -i "s|'\#\ 处理文件夹'|'niha'|g" a.sh
 
 
-sed -i 's|\# 处理文件夹|'niha'|g' a.sh
+sed -i 's|\# 处理文件夹|sed -i \"s\|\'sqlite:///\'+os.path.join\(SQL_PATH,\'kamifaka.db\'\)\|'\$\{DATABASE_URL/\'postgres\'/\'postgresql+psycopg2\'\}'\|g\" /usr/src/app/service/api/db.py|g' a.sh
+
+
+
+sed -i \"s\|'sqlite:///'+os.path.join(SQL_PATH,'kamifaka.db')\|'\${DATABASE_URL/'postgres'/'postgresql+psycopg2'}'\|g\" /usr/src/app/service/api/db.py
+
+
+sed -i "s|\# 处理文件夹|'niha'|g" a.sh
+sed -i "s|\# 处理文件夹|'niha'|g" a.sh
+
+sed -i "s|\# 处理文件夹|sed -i \"s\|'sqlite:///'+os.path.join(SQL_PATH,'kamifaka.db')\|'\${DATABASE_URL/'postgres'/'postgresql+psycopg2'}'\|g\" /usr/src/app/service/api/db.py|g" a.sh
+
+sed -i "s|\# 处理文件夹|sed -i \"s\|'sqlite:///'+os.path.join(SQL_PATH,'kamifaka.db')\|'\${DATABASE_URL8}'\|g\" /usr/src/app/service/api/db.py|g" a.sh
+
+
+
+
+sed -i "s|'sqlite:///'+os.path.join(SQL_PATH,'kamifaka.db')|'${DATABASE_URL/'postgres'/'postgresql+psycopg2'}'|g" /usr/src/app/service/api/db.py
+
+
+
+sed -i "s|'sqlite:///'+os.path.join(SQL_PATH,'kamifaka.db')|${DATABASE_URL/postgres'/'postgresql+psycopg2'}|g" /usr/src/app/service/api/db.py
+
+
+${DATABASE_URL/postgres/postgresql+psycopg2}
+
+
+sed -i 's|postgresql+psycopg2://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}|'${DATABASE_URL}'|g' /usr/src/app/docker-entrypoint.sh
+sed -i 's|postgres://|postgresql+psycopg2://|g' /usr/src/app/docker-entrypoint.sh
